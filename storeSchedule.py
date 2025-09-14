f4a5ad3cef1e6d4ec131794b088655af3173c91c
mingli202 <vincentmingli@gmail.com>
2025-09-14 13:07:49 -0400

    feat: schedules with total distance as weight

--- storeSchedule.py
+++ storeSchedule.py
@@ -5,6 +5,8 @@ from models import Location
 from data import load_locations
 import json
 import os
+import itertools
+from copy import deepcopy
 
 DAYS_IN_WEEK = 5
 
@@ -20,6 +22,10 @@ def haversine_km(l1: Location, l2: Location):
     ) * pow(math.sin(dlon / 2), 2)
     d = 2 * radius * math.asin(math.sqrt(a))
     return d
+
+
+def point_distance(p1: tuple[float, float], p2: tuple[float, float]) -> float:
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 
 def compute_distances(
@@ -116,56 +122,121 @@ def placeholder_distances() -> tuple[
             (home[1] - p_cur[1]) ** 2 + (home[0] - p_cur[0]) ** 2
         )
 
+    distances_from_home = OrderedDict(
+        sorted(distances_from_home.items(), key=lambda x: x[1])
+    )
+
     return pairwise_distances, distances_from_home
 
 
 def make_schedule(
     home: Location, locations: list[Location], important_locations: list[int]
-) -> list[list[list[int]]]:
+) -> list[tuple[list[list[int]], float]]:
     # pairwise_distances, distances_from_home = compute_distances(home, locations)
     pairwise_distances, distances_from_home = placeholder_distances()
 
     candidate_schedules: list[list[list[int]]] = [[]]
+    schedules: list[tuple[list[list[int]], float]] = []
 
-    i = 0
-    are_all_candidates_full = False
-    while not are_all_candidates_full:
-        are_all_candidates_full = True
+    pairs: set[tuple[int, ...]] = set()
 
-        for candidate in candidate_schedules:
+    def compute_closesest(
+        candidate: list[list[int]], ordered_distances: OrderedDict[int, float]
+    ) -> int:
+        closest_stores = list(ordered_distances.keys())
+
+        k = 0
+        closest_from_last_store = closest_stores[k]
+
+        scheduled_stores = set(itertools.chain.from_iterable(candidate))
+
+        while closest_from_last_store in scheduled_stores:
+            add_candidate(candidate, i, closest_from_last_store)
+
+            k += 1
+            closest_from_last_store = closest_stores[k]
+
+        return closest_from_last_store
+
+    def add_candidate(candidate: list[list[int]], i: int, closest_from_last_store: int):
+        another_candidate = deepcopy(candidate)
+
+        day_copy = deepcopy(another_candidate[i])
+        day_copy.append(closest_from_last_store)
+        day_copy.sort()
+        if tuple(day_copy) in pairs:
+            return
+
+        for dayy in another_candidate:
+            if closest_from_last_store in dayy:
+                dayy.remove(closest_from_last_store)
+                break
+
+        another_candidate[i].append(closest_from_last_store)
+
+        pairs.add(tuple(day_copy))
+
+        candidate_schedules.append(another_candidate)
+
+    while len(candidate_schedules) > 0:
+        candidate = candidate_schedules.pop()
+        is_candidate_full = False
+        i = 0
+
+        while not is_candidate_full:
             if len(candidate) <= i:
                 candidate.append([])
 
             day = candidate[i]
 
             if len(day) == 0:
-                if important := important_locations.pop():
+                if len(important_locations) > 0:
+                    important = important_locations.pop()
                     day.append(important)
                 else:
-                    closest_from_home = list(distances_from_home.keys())[0]
+                    closest_from_home = compute_closesest(
+                        candidate, distances_from_home
+                    )
                     day.append(closest_from_home)
-            else:
+            elif len(day) == 1:
                 last_store = day[-1]
-                closest_from_last_store = list(pairwise_distances[last_store].keys())[0]
+
+                closest_from_last_store = compute_closesest(
+                    candidate, pairwise_distances[last_store]
+                )
+
                 day.append(closest_from_last_store)
 
-            if any(len(day) != 2 for day in candidate):
-                are_all_candidates_full = False
+            pairs.add(tuple(day))
+            is_candidate_full = all(len(day) >= 2 for day in candidate)
 
-        i = (i + 1) % DAYS_IN_WEEK
+            i = (i + 1) % DAYS_IN_WEEK
 
-    return candidate_schedules
+        total_distance = 0
+        for day in candidate:
+            total_distance += (
+                sum(
+                    pow(pairwise_distances[day[i]][day[i + 1]], 2)
+                    for i in range(len(day) - 1)
+                )
+                + distances_from_home[day[0]]
+                + distances_from_home[day[-1]]
+            )
+
+        schedules.append((candidate, total_distance))
+
+    return sorted(schedules, key=lambda x: x[1])
 
 
 def main():
     locations = load_locations()
 
     home = Location(no=0, lat=45.461915521005885, lng=-73.87317833852978, index=0)
-    important_locations = [1, 18, 14, 12, 17]
+    important_locations = [1, 2]
 
     schedules = make_schedule(home, locations, important_locations)
 
-    print(f"schedules: {schedules}")
+    print("\n".join(map(json.dumps, schedules)))
 
 
 if __name__ == "__main__":
